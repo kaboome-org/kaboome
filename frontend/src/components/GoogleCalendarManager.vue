587cd46e73d06e0b7aff744bb3a4a34da24c611c
Lamaun <26430817+Lamaun@users.noreply.github.com>
2023-04-16 12:49:59 +0200

    Adjusted GoogleCalendarManager component to new backend interaction

--- frontend/src/components/GoogleCalendarManager.vue
+++ frontend/src/components/GoogleCalendarManager.vue
@@ -8,7 +8,19 @@
     >
       Authorize a Google Account
     </q-btn>
-    <q-tree :nodes="this.googleAccounts" no-nodes-label=" " node-key="label" default-expand-all>
+    <q-btn
+      color="blue"
+      text-color="white"
+      label="Sync now"
+      aria-label="Sync now"
+      v-on:click="this.syncNow()"
+    />
+    <q-tree
+      :nodes="this.googleAccounts"
+      no-nodes-label=" "
+      node-key="label"
+      default-expand-all
+    >
       <template v-slot:default-header="prop">
         <div class="row items-center">
           <q-icon :name="prop.node.icon || 'refresh'" class="q-mr-sm" />
@@ -17,13 +29,12 @@
       </template>
 
       <template v-slot:default-body="prop">
-        <div v-if="prop.node.calendarPath">
-          <q-btn
-            color="blue"
-            text-color="white"
-            label="Sync now"
-            aria-label="Sync now"
-            v-on:click="this.syncNow(prop.node.calendarPath)"
+        <div v-if="prop.node.doc">
+          <q-checkbox
+            v-model="prop.node.shouldSync"
+            label="Sync this calendar"
+            class="q-mr-md"
+            v-on:click="this.setShouldSync(prop.node)"
           />
         </div>
         <div v-else>
@@ -32,7 +43,7 @@
             text-color="white"
             label="Delete Account"
             aria-label="Delete Account"
-            onclick="alert('Not implemented yet.')"
+            onclick="alert('TODO: Not implemented yet.')"
           />
         </div>
       </template>
@@ -46,43 +57,42 @@ import { ref } from "vue";
 
 export default {
   methods: {
-    syncNow: function (googleCalendarPath) {
+    syncNow: function () {
       fetch("/backend/third-party-sync-events", {
         method: "POST",
-        body: JSON.stringify(googleCalendarPath),
         headers: {
           Accept: "application/json",
           "Content-Type": "application/json",
         },
       });
     },
-    loadGoogleAccounts: function () {
-      /* TODO: Load from userConfigDoc
-      fetch(`/backend/google-calendar-list`)
-        .then((res) => {
-          return res.text();
-        })
-        .then((t) => {
-          if (t !== "<h1>You must first authenticate.</h1>") {
-            const gaccs = JSON.parse(t);
-            this.googleAccounts = [];
-            for (const [key, value] of Object.entries(gaccs)) {
-              this.googleAccounts.push({
-                label: "google-" + key,
-                children: value.map((v) => {
-                  return {
-                    label: v.summary,
-                    icon: "calendar_month",
-                    calendarPath: {
-                      GoogleAccountId: key,
-                      GoogleCalendarId: v.summary,
-                    },
-                  };
-                }),
-              });
-            }
-          }
-        });*/
+    setShouldSync: function (node) {
+      node.doc.GoogleCalendarConfigs.find(
+        (v) => v.GoogleCalendarPath.GoogleCalendarId == node.label
+      ).ShouldSync = node.shouldSync;
+      this.config.configDb.put(node.doc);
+    },
+    loadGoogleAccounts: async function () {
+      const instance = this;
+      await this.config.configDb.allDocs(
+        { include_docs: true, descending: true },
+        function (err, doc) {
+          instance.googleAccounts = [];
+          doc.rows.forEach((row) => {
+            instance.googleAccounts.push({
+              label: row.id,
+              children: row.doc.GoogleCalendarConfigs.map((v) => {
+                return {
+                  label: v.GoogleCalendarPath.GoogleCalendarId,
+                  icon: "calendar_month",
+                  doc: row.doc,
+                  shouldSync: v.ShouldSync,
+                };
+              }),
+            });
+          });
+        }
+      );
     },
   },
   setup() {
@@ -94,7 +104,6 @@ export default {
     };
   },
   created() {
-    this.loadGoogleAccounts();
     this.config.activateSync(localStorage.getItem("user"));
     this.config.configDb
       .changes({
@@ -104,6 +113,7 @@ export default {
       .on("change", () => {
         this.loadGoogleAccounts();
       });
+    this.loadGoogleAccounts();
   },
 };
 </script>
