78cb2b7b4df51ab16fd93dfacea5cebb29f10fa3
Lamaun <26430817+Lamaun@users.noreply.github.com>
2023-06-25 18:22:56 +0200

    improved scheduler coverage and tests

--- frontend/test/vitest/__tests__/simpleScheduler.test.js
+++ frontend/test/vitest/__tests__/simpleScheduler.test.js
@@ -1,6 +1,6 @@
 import { describe, expect, it } from "vitest";
 import { schedule } from "src/schedulers/simpleScheduler";
-
+const MINUTE_MILLIS = 60 * 1000;
 describe("schedule method tests", () => {
   it("should schedule tasks and keep duration", () => {
     const tasks = [
@@ -15,6 +15,7 @@ describe("schedule method tests", () => {
     expect(tasks[0].end - tasks[0].start).toBe(duration);
     expect(tasks[0].commited).toBe(true);
     expect(tasks[0].start - new Date()).toBeGreaterThanOrEqual(0);
+    expect(tasks[0].start - new Date()).toBeLessThanOrEqual(10 * MINUTE_MILLIS);
   });
   it("should respect blockers", () => {
     const tasks = [
@@ -28,10 +29,55 @@ describe("schedule method tests", () => {
       {
         id: 1,
         start: new Date(),
-        end: new Date().valueOf() + 60000,
+        end: new Date().valueOf() + 60 * MINUTE_MILLIS,
       },
     ];
     schedule(tasks, blockers, { put: (_) => {} });
-    expect(tasks[0].start - new Date()).toBeGreaterThanOrEqual(60000);
+    expect(tasks[0].start - new Date()).toBeGreaterThanOrEqual(
+      60 * MINUTE_MILLIS
+    );
+    expect(tasks[0].start - new Date()).toBeLessThanOrEqual(70 * MINUTE_MILLIS);
+  });
+  it("should respect blockers", () => {
+    const tasks = [
+      {
+        id: 1,
+        start: new Date(2023, 3, 1, 9, 0),
+        end: new Date(2023, 3, 1, 10, 0),
+      },
+    ];
+    const blockers = [
+      {
+        id: 1,
+        start: new Date().valueOf() + 30 * MINUTE_MILLIS,
+        end: new Date().valueOf() + 90 * MINUTE_MILLIS,
+      },
+    ];
+    schedule(tasks, blockers, { put: (_) => {} });
+    expect(tasks[0].start - new Date()).toBeLessThanOrEqual(
+      100 * MINUTE_MILLIS
+    );
+    expect(tasks[0].start - new Date()).toBeGreaterThanOrEqual(
+      90 * MINUTE_MILLIS
+    );
+  });
+  it("should respect blockers", () => {
+    const tasks = [
+      {
+        id: 1,
+        start: new Date(2023, 3, 1, 9, 0),
+        end: new Date(2023, 3, 1, 10, 0),
+      },
+    ];
+    const blockers = [
+      {
+        id: 1,
+        start: new Date().valueOf() + 70 * MINUTE_MILLIS,
+        end: new Date().valueOf() + 90 * MINUTE_MILLIS,
+      },
+    ];
+    schedule(tasks, blockers, { put: (_) => {} });
+    expect(tasks[0].start - new Date()).toBeGreaterThanOrEqual(0);
+    expect(tasks[0].start - new Date()).toBeLessThanOrEqual(10 * MINUTE_MILLIS);
   });
 });
